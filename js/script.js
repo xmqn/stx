d3584d4e530e599c18833887e724d43533385951
박나영 <89637830+xmqn@users.noreply.github.com>
2021-12-21 02:47:57 +0900

    21.12.21
    모달창수정

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
+
+     let modal = $('.modal');
+    let modal_cont = $('.modal-cont');
+    let modal_close = $('.modal-close');
+    let modal_show = $('.modal-show');
+
+    modal.fadeIn();
+    modal_close.click(function () {
+        modal.fadeOut();
+    });
+
+    modal.click(function () {
+        modal.fadeOut();
+    });
+
+    modal_cont.click(function (event) {
+        event.stopPropagation();
+    });
+
     // 1. gnb 를 저장한다.
     var gnb = $('.gnb');
     // 2. 주메뉴 depth1
